7a44c2f0b79db28cea45f6159e1a85afec941a53
Rafał Rubaj <rrubaj@edu.cdv.pl>
2025-10-11 17:03:39 +0200

    zadanie 5 i 6

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// ZADANIE 5
 
-Console.WriteLine("Hello, World!");
+// KontoBankowe kontoBankowe = new KontoBankowe();
+//
+// kontoBankowe.Wplata(100);
+//
+// Console.WriteLine(kontoBankowe.PobierzSaldo());
+// kontoBankowe.Wyplata(50);
+// Console.WriteLine(kontoBankowe.PobierzSaldo());
+// kontoBankowe.Wyplata(55);
+//
+// class KontoBankowe
+// {
+//     private double saldo;
+//     public void Wplata(double kwota) { saldo += kwota; }
+//     public double PobierzSaldo() { return saldo; }
+//
+//     public void Wyplata(double kwota)
+//     {
+//         if (saldo >= kwota) saldo -= kwota;
+//         else Console.WriteLine("Nie wystarczające środki!");
+//     }
+// }
+
+// Pies pies = new Pies();
+// Kot kot = new Kot();
+//
+// pies.Szczekaj();
+// kot.Miaucz();
+//
+// class Zwierze
+// {
+//     public void Jedz() => Console.WriteLine("Zwierzę je");
+// }
+// class Pies : Zwierze
+// {
+//     public void Szczekaj() => Console.WriteLine("Hau hau!");
+// }
+//
+// class Kot : Zwierze
+// {
+//     public void Miaucz() => Console.WriteLine("Miau miau!");
+// }
