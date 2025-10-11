f2351909c9846448e45e1d61337ee1a984084d30
Rafał Rubaj <rrubaj@edu.cdv.pl>
2025-10-11 16:20:44 +0200

    zadanie 2

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,31 +1,46 @@
-﻿const int requiredAge = 14;
-const int simRequiredAge = 18;
-const string accessDenied = "Musisz mieć 18 lat.";
-const string accessAllowed = "Witamy w naszym sklepie";
-const string simRestrictionMessage = "Witamy w naszym sklepie, musisz mieć 18 lat aby zakupić kartę sim";
+﻿// ZADANIE 1
 
-Console.WriteLine("Podaj swój wiek: ");
+// const int requiredAge = 14;
+// const int simRequiredAge = 18;
+// const string accessDenied = "Musisz mieć 18 lat.";
+// const string accessAllowed = "Witamy w naszym sklepie";
+// const string simRestrictionMessage = "Witamy w naszym sklepie, musisz mieć 18 lat aby zakupić kartę sim";
+//
+// Console.WriteLine("Podaj swój wiek: ");
+//
+// string? input = Console.ReadLine();
+//
+// bool success = int.TryParse(input, out var age);
+//
+// if (!success)
+// {
+//     Console.WriteLine("Podaj poprawną wartość!");
+// }
+// else
+// {
+//     if (age >= simRequiredAge)
+//     {
+//         Console.WriteLine(accessAllowed);
+//     }
+//     else if (age >= requiredAge)
+//     {
+//         Console.WriteLine(simRestrictionMessage);
+//     }
+//     else
+//     {
+//         Console.WriteLine(accessDenied);
+//     }
+// }
 
-string? input = Console.ReadLine();
 
-bool success = int.TryParse(input, out var age);
+// ZADANIE 2
 
-if (!success)
+var input = "";
+
+while (input != "admin123")
 {
-    Console.WriteLine("Podaj poprawną wartość!");
+    Console.WriteLine("Podaj hasło: ");
+    input = Console.ReadLine();
 }
-else
-{
-    if (age >= simRequiredAge)
-    {
-        Console.WriteLine(accessAllowed);
-    }
-    else if (age >= requiredAge)
-    {
-        Console.WriteLine(simRestrictionMessage);
-    }
-    else
-    {
-        Console.WriteLine(accessDenied);
-    }
-}
+
+Console.WriteLine("Zalogowano pomyślnie!");
